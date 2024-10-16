851886b8f335392d9b40bcd00553ecb49fdd035b
Iker BEDECARRAX <iker.bedecarrax@ekino.com>
2024-10-16 13:37:03 +0200

    style(languages): put items in row on monitor

--- libs/languages/src/lib/languages/languages.component.ts
+++ libs/languages/src/lib/languages/languages.component.ts
@@ -4,12 +4,12 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ProgressItem } from './progress-item.model';
 
 @Component({
-  standalone: true,
   selector: 'cv-languages',
+  standalone: true,
+  imports: [CommonModule, MatProgressSpinnerModule],
   templateUrl: './languages.component.html',
   styleUrls: ['./languages.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, MatProgressSpinnerModule],
 })
 export class LanguagesComponent {
   languages: readonly ProgressItem[] = [
